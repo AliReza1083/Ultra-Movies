05b0a7f9005802d9a59a46d9922d76c5fb17c6b4
Ali Reza <webdeve1083@gmail.com>
2022-11-29 17:54:31 +0800

    The user can add movie from now on...will style it later

--- src/routes/Add.jsx
+++ src/routes/Add.jsx
@@ -1,5 +1,45 @@
+import { db } from "../utils/Add";
+import { addDoc, collection } from "firebase/firestore";
+
 const Add = () => {
-  return <div className="display">Add</div>;
+  const colRef = collection(db, "movies");
+
+  const addingData = (e) => {
+    e.preventDefault();
+    const createdAt = new Date();
+
+    addDoc(colRef, {
+      name: e.target.name.value,
+      country: e.target.country.value,
+      year: e.target.year.valueAsNumber,
+      img: e.target.img.value,
+      trailer: e.target.trailer.value,
+      details: e.target.details.value,
+      createdAt: createdAt,
+    }).then(() => {
+      console.log("added");
+    });
+  };
+
+  return (
+    <div className="display">
+      <form action="" className="text-black" onSubmit={addingData}>
+        <input type="text" name="name" placeholder="name" required />
+        <input type="text" name="country" placeholder="country" required />
+        <input type="number" name="year" placeholder="year" required />
+        <input type="url" name="img" placeholder="img" required />
+        <input type="url" name="trailer" placeholder="trailer" required />
+        <textarea
+          name="details"
+          cols="30"
+          rows="10"
+          placeholder="details"
+          required
+        ></textarea>
+        <button>add</button>
+      </form>
+    </div>
+  );
 };
 
 export default Add;
